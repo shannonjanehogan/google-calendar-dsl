81a38211e8e01dd4a06956b384638a6b6a00bf72
shannonjanehogan <shannon.jane.hogan@gmail.com>
2019-10-02 16:04:14 -0700

    Create all classes and folders

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,7 @@
 console.log("Hello world");
+
+import { CalendarProgram } from "./CalendarProgram";
+
+
+let calendarProgram = new CalendarProgram("./inputs/valid_simple.cal");
+calendarProgram.tokenize();
